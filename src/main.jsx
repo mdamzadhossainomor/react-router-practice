743eaf78bc18094cb5f6080d5676ecbcb758a94d
mdamzadhossainomor <mdamzadhossainomor@gmail.com>
2025-12-24 03:00:28 +0600

    main jsx uncomment

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,39 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import { createBrowserRouter, RouterProvider } from "react-router";
-// import App from "./App.jsx";
-// import { About } from "./components/About/About.jsx";
-// import { Trams } from "./components/Trams/Trams.jsx";
-// import "./index.css";
-// import { AllUsers} from "./components/Allusers/AllUsers.jsx";
-// import { ParamsData } from "./components/paramsData/ParamsData.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from "./App.jsx";
+import { About } from "./components/About/About.jsx";
+import { Trams } from "./components/Trams/Trams.jsx";
+import "./index.css";
+import { AllUsers} from "./components/Allusers/AllUsers.jsx";
+import { ParamsData } from "./components/paramsData/ParamsData.jsx";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     Component: App,
-//   },
-//   {
-//     path: "/about",
-//     Component: About,
-//   },
-//   {
-//     path: "/trams",
-//     Component: Trams,
-//   },
-//   {
-//     path: "/users",
-//     Component: AllUsers,
-//   },
-//   {
-//     path: "/users/:userId",
-//     loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
-//     Component: ParamsData,
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+  {
+    path: "/about",
+    Component: About,
+  },
+  {
+    path: "/trams",
+    Component: Trams,
+  },
+  {
+    path: "/users",
+    Component: AllUsers,
+  },
+  {
+    path: "/users/:userId",
+    loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
+    Component: ParamsData,
+  },
+]);
 
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <RouterProvider router={router} />
-//   </StrictMode>
-// );
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
