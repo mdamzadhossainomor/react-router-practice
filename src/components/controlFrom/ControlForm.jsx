86031d717ab27f20fe2a1fde1966f610a3f22d51
mdamzadhossainomor <mdamzadhossainomor@gmail.com>
2025-12-24 18:28:08 +0600

    control data input

--- src/components/controlFrom/ControlForm.jsx
+++ src/components/controlFrom/ControlForm.jsx
@@ -1,15 +1,43 @@
-export const ControlForm = () => {
-    const handleSubmit = (event) => { 
-        event.preventDefault()
-        console.log(event.target.name.value);
-        console.log(event.target.email.value);
-    }
+import { useState } from "react";
 
+export const ControlForm = () => {
+  //   const handleSubmit = (event) => {
+  //     event.preventDefault();
+  //     console.log(event.target.name.value);
+  //     console.log(event.target.email.value);
+  //   };
+  const [formData, setFormData] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.email.value);
+  };
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
   return (
+    // <div>
+    //   <form onSubmit={handleSubmit}>
+    //     <label htmlFor="name">Name:</label>
+    //     <input type="text" id="name" name="name" />
+    //     <br />
+    //     <label htmlFor="email">Email:</label>
+    //     <input type="email" id="email" name="email" />
+    //     <br />
+    //     <button type="submit">Submit</button>
+    //   </form>
+    // </div>
     <div>
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          defaultValue={formData}
+          onChange={handleChange}
+        />
         <br />
         <label htmlFor="email">Email:</label>
         <input type="email" id="email" name="email" />
